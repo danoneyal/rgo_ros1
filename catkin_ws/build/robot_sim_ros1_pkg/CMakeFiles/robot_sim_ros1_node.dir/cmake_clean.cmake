0af69590051b0983cfbc2f04a59783ec0ed40629
danon eyal <danoneyal@gmail.com>
2022-08-20 07:00:12 -0700

    ros1 robot sim

---
+++ catkin_ws/build/robot_sim_ros1_pkg/CMakeFiles/robot_sim_ros1_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ros-industrial/rgo/ros1/catkin_ws/devel/lib/robot_sim_ros1_pkg/robot_sim_ros1_node"
+  "/home/ros-industrial/rgo/ros1/catkin_ws/devel/lib/robot_sim_ros1_pkg/robot_sim_ros1_node.pdb"
+  "CMakeFiles/robot_sim_ros1_node.dir/src/robot_sim_ros1_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_sim_ros1_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/robot_sim_ros1_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros-industrial/rgo/ros1/catkin_ws/src/robot_sim_ros1_pkg
+# Build directory: /home/ros-industrial/rgo/ros1/catkin_ws/build/robot_sim_ros1_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
